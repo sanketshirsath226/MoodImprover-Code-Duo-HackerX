744a7b4313eef859aae0453d91b13e169a89bc9f
Sanket Shirsath <sanketshirsath226@gmail.com>
2021-10-09 13:52:45 +0530

    Deleted File succesfull if exists

--- Emotion-Recognition-API/upload.php
+++ Emotion-Recognition-API/upload.php
@@ -1,51 +1,35 @@
 <?php
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-    
-    // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-      $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-      if($check == false) {
-       // echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-        print_r($check);
-      } 
-      else {
-        echo "File is image.";
-        $uploadOk = 0;
-      }
-    }
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-    /*// Check if file already exists
-    if (file_exists($target_file)) {
-      echo "Sorry, file already exists.";
-      $uploadOk = 0;
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    if(!unlink($target_file))
+    {
+        $uploadOk = 0;
     }
-    
-    // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 500000) {
-      echo "Sorry, your file is too large.";
-      $uploadOk = 0;
-    }
-    
-    // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-      $uploadOk = 0;
-    }
-    */
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-      echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
+}
+  // Check file size
+  if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+  }
+  
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+  // if everything is ok, try to upload file
+  } else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        header("Location:facedecode.php?username={$_FILES["fileToUpload"]["name"]}");
     } else {
-      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-      } else {
-        echo "Sorry, there was an error uploading your file.";
-      }
+      echo "Sorry, there was an error uploading your file.";
     }
+  }
+}
 ?>
